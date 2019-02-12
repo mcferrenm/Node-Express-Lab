758a9a5f27897986f5b5a3a727b3e665221951a1
mcferrenm <mcferrenmax@gmail.com>
2019-02-12 15:21:28 -0500

    Add and initialize posts router

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const postsRouter = require('./posts/posts-router.js')
+
 const server = express();
+
+server.use("/api/posts", postsRouter)
 
 server.get('/', (req, res) => {
   res.status(200).send("<h1>Welcome to Max's Server</h1>")
