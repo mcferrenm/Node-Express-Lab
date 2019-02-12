2b214e6d678c9e6271812c1c533d1b891c2111c8
mcferrenm <mcferrenmax@gmail.com>
2019-02-12 15:24:47 -0500

    Convert server into module

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require('express');
-
-const postsRouter = require('./posts/posts-router.js')
-
-const server = express();
-
-server.use("/api/posts", postsRouter)
-
-server.get('/', (req, res) => {
-  res.status(200).send("<h1>Welcome to Max's Server</h1>")
-})
+const server = require('./server.js')
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router.js')
+
+const server = express();
+
+server.use("/api/posts", postsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).send("<h1>Welcome to Max's Server</h1>")
+})
+
+module.exports = server;
