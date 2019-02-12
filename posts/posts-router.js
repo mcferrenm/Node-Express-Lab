3771fa8ac13bd62aa310193728a0fb599b5a1891
mcferrenm <mcferrenmax@gmail.com>
2019-02-12 15:47:22 -0500

    Add POST post (newPost) route

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,7 +16,7 @@ router.get('/:id', async (req, res) => {
   const postId = req.params.id;
   try {
     const post = await db.findById(postId);
-    if(post.length > 0) {
+    if (post.length > 0) {
       res.status(200).json(post)
     } else {
       res.status(404).json({ message: "The post with the specified ID does not exist." })
@@ -26,16 +26,28 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  const newPost = req.body;
+  if(!newPost.title || !newPost.contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  } else {
+    try {
+      const newId = await db.insert(newPost)
+      const createdPost = await db.findById(newId.id)
+      res.status(201).json(createdPost)
+    } catch (error) {
+      res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
+  }
+
 });
 
 router.delete('/', (req, res) => {
-  
+
 });
 
 router.put('/', (req, res) => {
-  
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const postsRouter = require('./posts/posts-router.js')
 
 const server = express();
 
+server.use(express.json());
+
 server.use("/api/posts", postsRouter)
 
 server.get('/', (req, res) => {
