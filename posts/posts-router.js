d3dfef7431de9c5f4793f9800f6a544c61911841
mcferrenm <mcferrenmax@gmail.com>
2019-02-12 15:31:06 -0500

    Add GET posts route

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const db = require('../data/db.js')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send("<h1>Welcome to the Posts Endpoint</h1>")
+router.get('/', async (req, res) => {
+  try {
+    const posts = await db.find()
+    res.status(200).json(posts)
+  } catch (error) {
+    res.status(500).json({ error: "The posts information could not be retrieved." })
+  }
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+  
+});
+
+router.delete('/', (req, res) => {
+  
+});
+
+router.put('/', (req, res) => {
+  
 });
 
 module.exports = router;
